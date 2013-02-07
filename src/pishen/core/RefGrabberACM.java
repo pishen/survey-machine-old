412ad4233a6d895f20c13a95101525c5d715483a
pishen <pishen.02@gmail.com>
2013-02-08 01:13:14 +0800

    debug

--- src/pishen/core/RefGrabberACM.java
+++ src/pishen/core/RefGrabberACM.java
@@ -23,6 +23,7 @@ public class RefGrabberACM {
 	}
 	
 	public void grabRef() throws DownloadFailException, IOException{
+		log.info("grabbing");
 		URL refURL = new URL(dbRecord.getStringProperty(Key.EE) + "&preflayout=flat");
 		ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
 		
