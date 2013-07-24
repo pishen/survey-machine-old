dde37da060d4e8688c341667a0df84722dc84ec2
pishen <pishen02@gmail.com>
2013-07-25 00:10:35 +0800

    implement MAP

--- src/pishen/core/MAPComputer.java
+++ src/pishen/core/MAPComputer.java
@@ -1,85 +1,34 @@
 package pishen.core;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
+
+import pishen.db.Record;
+
 
 public class MAPComputer {
-	private HashMap<String, HashSet<String>> ansSetMap = new HashMap<String, HashSet<String>>();
-	private ArrayList<Double> apList = new ArrayList<Double>();
 	private int threshold;
 	
 	public MAPComputer(int threshold){
 		this.threshold = threshold;
 	}
 	
-	public double computeMap(File answerList, File rankList) throws IOException{
-		fillAnsSetMap(answerList);
-		parseRankList(rankList);
-		
+	public double computeMAPOn(List<TestCase> testCases){
 		double apSum = 0.0;
-		for(double ap: apList){
-			apSum += ap;
+		for(TestCase testCase: testCases){
+			apSum += computeAPOn(testCase.getRankRecords(), testCase.getAnsRecords());
 		}
-		return apSum / (double)ansSetMap.size();
+		return apSum / (double)testCases.size();
 	}
 	
-	private void parseRankList(File rankList) throws IOException{
-		ArrayList<String> singleRankList = null;
-		String topic = null;
-		BufferedReader in = new BufferedReader(new FileReader(rankList));
-		String line = null;
-		try {
-			while((line = in.readLine()) != null){
-				String[] elements = line.split(" ");
-				if(elements.length != 2){
-					//wrong ranked list format
-					//throw new FormatException();
-				}
-				if(!elements[0].equals(topic)){
-					computeAP(topic, singleRankList);
-					topic = elements[0];
-					singleRankList = new ArrayList<String>();
-				}
-				singleRankList.add(elements[1]);
-			}
-			computeAP(topic, singleRankList);
-		} finally {
-			in.close();
-		}
-	}
-	
-	private void computeAP(String topic, ArrayList<String> singleRankList){
-		if(singleRankList == null || ansSetMap.get(topic) == null){
-			return;
-		}
-		
+	private double computeAPOn(List<Record> rankRecords, List<Record> ansRecords){
 		int matchCount = 0;
 		double precisionSum = 0.0;
-		for(int i = 0; i < singleRankList.size() && i < threshold; i++){
-			String doc = singleRankList.get(i);
-			if(ansSetMap.get(topic).contains(doc)){
-				++matchCount;
+		for(int i = 0; i < rankRecords.size() && i < threshold; i++){
+			if(ansRecords.contains(rankRecords.get(i))){
+				matchCount++;
 				precisionSum += matchCount / (double)(i + 1);
 			}
 		}
-		apList.add(precisionSum / (double)(ansSetMap.get(topic).size()));
-	}
-	
-	private void fillAnsSetMap(File answerList) throws IOException{
-		BufferedReader in = new BufferedReader(new FileReader(answerList));
-		String line = null;
-		while((line = in.readLine()) != null){
-			String[] elements = line.split("\\s");
-			if(!ansSetMap.containsKey(elements[0])){
-				ansSetMap.put(elements[0], new HashSet<String>());
-			}
-			ansSetMap.get(elements[0]).add(elements[1]);
-		}
-		in.close();
+		return precisionSum / (double)ansRecords.size();
 	}
 }
